f0fff4528a4beca8a7d1dd825935cfde73de4ef8
elangenbucher <elangenbucher@deloitte.com>
2021-11-12 12:58:02 +0100

    extended admin view for user model #16

--- backend/core/user/admin.py
+++ backend/core/user/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import User
 from .viewsets import UserViewSet
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'institute', 'is_staff', 'date_joined')
+
+
 # Register your models here.
-admin.site.register(User)
-#admin.register(User, UserViewSet)
+admin.site.register(User, UserAdmin)
