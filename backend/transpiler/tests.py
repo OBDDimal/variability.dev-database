ea2a8faf57e262cb0986152178af7dc6d69e94e5
elangenbucher <elangenbucher@deloitte.com>
2021-12-23 10:44:50 +0100

    update graph vis

--- backend/transpiler/tests.py
+++ backend/transpiler/tests.py
@@ -17,7 +17,7 @@ path = f"{Path(__file__).resolve().parent}{os.path.sep}xmlExamples{os.path.sep}"
 write_to_file(json.dumps(xml_to_json(path + 'npc.xml'), indent=2), f"{path}model_npc.json")
 
 # ----- to G6
-# write_to_file(json.dumps(xml_to_g6(path + 'BerkeleyDB.xml'), indent=2), f"{path}g6_model.json")
-# write_to_file(json.dumps(xml_to_g6(path + 'oldsmall.xml'), indent=2), f"{path}g6_model_small.json")
-# write_to_file(json.dumps(xml_to_g6(path + 'Automotive02v04.xml'), indent=2), f"{path}g6_model_big.json")
+write_to_file(json.dumps(xml_to_g6(path + 'BerkeleyDB.xml'), indent=2), f"{path}g6_model.json")
+write_to_file(json.dumps(xml_to_g6(path + 'oldsmall.xml'), indent=2), f"{path}g6_model_small.json")
+write_to_file(json.dumps(xml_to_g6(path + 'Automotive02v04.xml'), indent=2), f"{path}g6_model_big.json")
 write_to_file(json.dumps(xml_to_g6(path + 'npc.xml'), indent=2), f"{path}g6_model_npc.json")
