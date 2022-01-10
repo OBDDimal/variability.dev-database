39b1e31aec12bac8a732a4381e389400105a0bb7
Eixix <tobiasbetz@hotmail.de>
2022-01-10 15:58:02 +0100

    WIP: Class based componenet

--- frontend/src/routes/Files/FileEdit.tsx
+++ frontend/src/routes/Files/FileEdit.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Select from "react-select";
@@ -6,132 +6,150 @@ import api from "../../services/api.service";
 
 const API_URL = process.env.REACT_APP_DOMAIN;
 
-type fileType = {
+type Props = {};
+
+type State = {
+  id?: number;
+  file?: {
+    label: string;
+    description: string;
+    new_version_of: number;
+    tags: [
+      {
+        id: number;
+        label: string;
+        description: string;
+      }
+    ];
+  };
   label?: string;
   description?: string;
-  tags?: [{ id: number; label: string }];
-  new_version_of?: number;
+  gottenTags: Array<{ label: string; value: string }>;
+  gottenFiles: Array<{ value: number; label: string }>;
+  newVersionOf?: string;
+  tags?: string;
 };
 
-function FileEdit() {
-  const { id } = useParams<"id">();
-  const [fileState, setFileState] = useState([] as fileType);
+export default class FileEdit extends Component<Props, State> {
+  constructor(props: Props | Readonly<Props>) {
+    super(props);
+    const url = window.location.pathname;
+    const id = url.substring(url.lastIndexOf("/") + 1);
 
-  const [labelState, setLabelState] = useState("");
-  const [descriptionState, setDescriptionState] = useState("");
-  const [gottenFilesState, setGottenFilesState] = useState(
-    [] as Array<{ value: string; label: string }>
-  );
-  const [gottenTagsState, setGottenTagsState] = useState(
-    [] as Array<{ value: string; label: string }>
-  );
-  const [newVersionOfState, setNewVersionOfState] = useState("");
-  const [tagsState, setTagsState] = useState("");
-
-  useEffect(() => {
     api.get(`${API_URL}files/${id}/`).then((response) => {
-      setFileState(response.data);
-      getNewVersionOf();
-      getTags();
+      this.setState({ file: response.data });
+      this.getNewVersionOf();
+      this.getTags();
     });
-  }, [id]);
+  }
 
-  const getTags = () => {
+  state: State = {
+    id: undefined,
+    file: undefined,
+    label: undefined,
+    description: undefined,
+    gottenTags: [],
+    gottenFiles: [],
+    newVersionOf: "---",
+  };
+
+  getTags = () => {
     api.get(API_URL + "tags/").then((response) => {
       let tags = response.data.results;
       tags = tags.map((tag: { id: number; label: string }) => {
-        return { value: tag.id.toString(), label: tag.label };
+        return { value: tag.id, label: tag.label };
       });
-      setGottenTagsState(tags);
+      this.setState({ gottenTags: tags });
     });
   };
 
-  const getNewVersionOf = () => {
+  getNewVersionOf = () => {
     api.get(API_URL + "files/").then((response) => {
       let files = response.data.results;
       files = files.map((file: { id: number; label: string }) => {
-        return { value: file.id.toString(), label: file.label };
+        return { value: file.id, label: file.label };
       });
-      setGottenFilesState(files);
+      this.setState({ gottenFiles: files });
     });
   };
 
-  const onLabelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const label = e.target as HTMLInputElement;
-    setLabelState(label.value);
-  };
-
-  const onDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const description = e.target as HTMLInputElement;
-    setDescriptionState(description.value);
-  };
-
-  const onNewVersionOfChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newVersionOf = e.target as HTMLSelectElement;
-    setNewVersionOfState(newVersionOf.value);
-  };
-
-  const onTagChange = (options: any) => {
-    setTagsState(
-      options.map((option: any) => {
+  onTagChange = (options: any) => {
+    this.setState({
+      tags: options.map((option: any) => {
         return { id: option.value, label: option.label };
-      })
-    );
+      }),
+    });
+  };
+
+  onLabelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const label = e.target as HTMLInputElement;
+    this.setState({ label: label.value });
+  };
+
+  onDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const description = e.target as HTMLInputElement;
+    this.setState({ description: description.value });
+  };
+
+  onNewVersionOfChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newVersionOf = e.target as HTMLSelectElement;
+    this.setState({ newVersionOf: newVersionOf.value });
   };
 
   // const onSubmit = () => {};
 
   // label, description, tags, new_version_of
-  return (
-    <>
-      <Form.Group className='mb-3'>
-        <Form.Label>File name</Form.Label>
-        <Form.Control
-          data-testid='label'
-          onChange={onLabelChange}
-          placeholder='Leave a filename'
-          value={fileState.label}
-        />
-      </Form.Group>
-      <Form.Group className='mb-3'>
-        <Form.Label>Description</Form.Label>
-        <Form.Control
-          data-testid='description'
-          as='textarea'
-          onChange={onDescriptionChange}
-          placeholder='Leave a comment here'
-          value={fileState.description}
-        />
-      </Form.Group>
-      <Form.Group className='mb-3'>
-        <Form.Label>New version of</Form.Label>
-        <Form.Select
-          onChange={onNewVersionOfChange}
-          defaultValue={fileState.new_version_of}
-        >
-          {gottenFilesState.map((key) => {
-            return (
-              <option key={key.value} value={key.value}>
-                {key.value}: {key.label}
-              </option>
-            );
-          })}
-          <option key='---' value='---'>
-            ---
-          </option>
-        </Form.Select>
+
+  render() {
+    return (
+      <>
         <Form.Group className='mb-3'>
-          <Form.Label>Tags</Form.Label>
-          <Select
-            defaultValue={{ key: "Tobi", value: "1" }}
-            isMulti
-            onChange={onTagChange}
-            options={[{ key: "Tobi", value: "1" }]}
+          <Form.Label>File name</Form.Label>
+          <Form.Control
+            data-testid='label'
+            onChange={this.onLabelChange}
+            placeholder='Leave a filename'
+            value={this.state.file?.label}
           />
         </Form.Group>
-      </Form.Group>
-    </>
-  );
+        <Form.Group className='mb-3'>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            data-testid='description'
+            as='textarea'
+            onChange={this.onDescriptionChange}
+            placeholder='Leave a comment here'
+            value={this.state.file?.description}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3'>
+          <Form.Label>New version of</Form.Label>
+          <Form.Select
+            onChange={this.onNewVersionOfChange}
+            defaultValue={this.state.file?.new_version_of}
+          >
+            {this.state.gottenFiles.map((key) => {
+              return (
+                <option key={key.value} value={key.value}>
+                  {key.value}: {key.label}
+                </option>
+              );
+            })}
+            <option key='---' value='---'>
+              ---
+            </option>
+          </Form.Select>
+          <Form.Group className='mb-3'>
+            <Form.Label>Tags</Form.Label>
+            <Select
+              defaultValue={[this.state.gottenTags[0]]}
+              isMulti
+              onChange={this.onTagChange}
+              options={this.state.gottenTags}
+            />
+          </Form.Group>
+        </Form.Group>
+      </>
+    );
+  }
 }
-
-export default FileEdit;
