dc6c6d8b6f474a3f99a3ebbb0409640728ecffd4
Eixix <tobiasbetz@hotmail.de>
2022-01-19 14:11:28 +0100

    Deleted unnecessary file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,6 @@
 
 while !</dev/tcp/db/5432; do echo "Connecting" && sleep 1; done;
 python manage.py makemigrations
-python manage.py migrate --run-syncdb 
+python manage.py migrate --run-syncdb
 uwsgi --module ddueruemweb.wsgi --master --processes 1 --threads 1 --socket /tmp/uwsgi.sock --chmod-socket=666 &
 nginx -g "daemon off;"
