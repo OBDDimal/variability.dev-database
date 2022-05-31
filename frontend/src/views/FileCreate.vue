414ec3e83de08ef0d9567102d77d0fd0a86209e9
Lukas Harsch <lukas.harsch@uni-ulm.de>
2022-05-31 18:57:44 -0500

    File upload boilerplate

--- frontend/src/views/FileCreate.vue
+++ frontend/src/views/FileCreate.vue
@@ -4,51 +4,55 @@
         <h5 class="text-h5 mb-4">
             Use this form to upload a new Feature Model publicly
         </h5>
-        <v-row>
-            <v-col cols="12" md="6">
-                <v-text-field v-model="editedItem.label" label="Label"></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-                <v-text-field v-model="editedItem.description" label="Description">
-                </v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-                <v-file-input chips multiple label="File Upload" show-size></v-file-input>
-            </v-col>
-            <v-col cols="12" md="6">
-                <v-autocomplete v-model="editedItem.license" :items="licenses" label="License"></v-autocomplete>
-            </v-col>
-            <v-col cols="12" md="6">
+        <v-form v-model="valid">
+            <v-row>
+                <v-col cols="12" md="6">
+                    <v-text-field v-model="label" label="Label" placeholder="Leave a filename"></v-text-field>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-text-field v-model="description" label="Description" placeholder="Leave a comment here"
+                        counter="250" :rules="descriptionRules">
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-file-input chips label="File Upload" accept=".xml" show-size></v-file-input>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-autocomplete v-model="license" :item-text="gottenLicenses.map(function (a) { return a.label; })"
+                        :item-value="gottenLicenses.map(function (a) { return a.value; })" label="License">
+                    </v-autocomplete>
+                </v-col>
+                <!-- <v-col cols="12" md="6">
                 <v-autocomplete :disabled="newFamily != ''" v-model="family" :items="existingFamilies"
                     label="New version of"></v-autocomplete>
             </v-col>
             <v-col cols="12" md="6">
                 <v-text-field :disabled="family != null" v-model="newFamily" label="New family"></v-text-field>
-            </v-col>
-            <v-col cols="12">
-                <v-combobox v-model="editedItem.tags" :items="tags" label="Tags" multiple chips></v-combobox>
-            </v-col>
-            <v-col cols="12">
-                <v-checkbox v-model="check1" label="Lorem Ipsum" hide-details></v-checkbox>
-            </v-col>
-            <v-col cols="12">
-                <v-checkbox v-model="check2" label="Lorem Ipsum 2" hide-details>
-                </v-checkbox>
-            </v-col>
-            <v-col cols="12">
-                <v-checkbox v-model="check3" label="Lorem Ipsum 3" hide-details>
-                </v-checkbox>
-            </v-col>
-            <v-col cols="12">
-                <v-btn color="primary" :disabled="
-                    !check1 ||
-                    !check2 ||
-                    !check3
-                ">Upload</v-btn>
-            </v-col>
-        </v-row>
+            </v-col> -->
+                <v-col cols="12">
+                    <v-combobox v-model="tags" :items="gottenTags.map(function (a) { return a.value; })" label="Tags"
+                        multiple chips></v-combobox>
+                </v-col>
+                <v-col cols="12">
+                    <v-checkbox v-model="legalShare" label="I am legally allowed to share this model" class="mt-0" hide-details>
+                    </v-checkbox>
+                </v-col>
+                <v-col cols="12">
+                    <v-checkbox v-model="userData" label="My email and a date will always be tied to the file
+                 upload (even after account deletion)" class="mt-0" hide-details>
+                    </v-checkbox>
+                </v-col>
+                <v-col cols="12">
+                    <v-checkbox v-model="openSource" label="All information will be published according to your
+                 chosen license" class="mt-0" hide-details>
+                    </v-checkbox>
+                </v-col>
+                <v-col cols="12">
+                    <v-btn color="primary" :disabled="!valid">Upload</v-btn>
+                </v-col>
+            </v-row>
+        </v-form>
     </div>
-
 </template>
 
 <script lang="ts">
@@ -63,8 +67,10 @@ export default Vue.extend({
     props: {},
 
     data: () => ({
+        valid: false,
         label: "" as NewFile["label"],
         description: "" as NewFile["description"],
+        descriptionRules: [(v: any) => v.length <= 250 || 'Max 250 characters please'],
         file: undefined as NewFile["file"],
         gottenLicenses: [] as NewFile["gottenLicenses"],
         gottenTags: [] as NewFile["gottenTags"],
@@ -83,7 +89,7 @@ export default Vue.extend({
     }),
 
     computed: {
-        
+
     },
 
     methods: {
