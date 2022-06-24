4cdc006c512403f53f3140dd1135b8d25bd66a4c
Lukas Harsch <lukas.harsch@uni-ulm.de>
2022-06-24 15:37:58 +0200

    Undo merge of backend from dev, works now again

--- backend/ddueruemweb/settings.py
+++ backend/ddueruemweb/settings.py
@@ -133,7 +133,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.renderers.JSONRenderer',
         # enable django rest framework rendering in browser
         'rest_framework.renderers.BrowsableAPIRenderer',
