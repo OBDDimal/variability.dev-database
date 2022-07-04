bed5bddb130aead72f7849aa40973b3b93957d61
Simon Karrer <simonkrrr3@googlemail.com>
2022-07-04 13:57:08 +0200

    Added enter on edit.

--- frontend/src/components/FeatureModel/FeatureModelTreeEditDialog.vue
+++ frontend/src/components/FeatureModel/FeatureModelTreeEditDialog.vue
@@ -4,44 +4,46 @@
 			<v-card>
 				<v-card-title class="text-h5 grey lighten-2"> Edit Node </v-card-title>
 
-				<v-card-text>
-					<v-row class="my-2">
-						<v-col cols="12">
-							<template>
-								<v-text-field v-model="name" :rules="[(value) => !!value || 'Required.']"></v-text-field>
-							</template>
-						</v-col>
-					</v-row>
+				<v-form @submit.prevent="confirm">
+					<v-card-text>
+						<v-row class="my-2">
+							<v-col cols="12">
+								<template>
+									<v-text-field v-model="name" :rules="[(value) => !!value || 'Required.']"></v-text-field>
+								</template>
+							</v-col>
+						</v-row>
 
-					<v-row class="my-2">
-						<v-col cols="12">
-							<v-btn-toggle v-if="showGroupTypeSelection" v-model="groupType" mandatory>
-								<v-btn>⊻ alt </v-btn>
-								<v-btn>∨ or </v-btn>
-								<v-btn>∧ and </v-btn>
-							</v-btn-toggle>
-						</v-col>
-					</v-row>
+						<v-row class="my-2">
+							<v-col cols="12">
+								<v-btn-toggle v-if="showGroupTypeSelection" v-model="groupType" mandatory>
+									<v-btn>⊻ alt </v-btn>
+									<v-btn>∨ or </v-btn>
+									<v-btn>∧ and </v-btn>
+								</v-btn-toggle>
+							</v-col>
+						</v-row>
 
-					<v-row class="my-2">
-						<v-col cols="12">
-							<v-btn-toggle v-if="showMandatorySelection" v-model="mandatory" mandatory>
-								<v-btn> mandatory </v-btn>
-								<v-btn> optional </v-btn>
-							</v-btn-toggle>
-						</v-col>
-					</v-row>
+						<v-row class="my-2">
+							<v-col cols="12">
+								<v-btn-toggle v-if="showMandatorySelection" v-model="mandatory" mandatory>
+									<v-btn> mandatory </v-btn>
+									<v-btn> optional </v-btn>
+								</v-btn-toggle>
+							</v-col>
+						</v-row>
 
-					<v-checkbox v-model="abstract" label="Abstract" hide-details></v-checkbox>
-				</v-card-text>
+						<v-checkbox v-model="abstract" label="Abstract" hide-details></v-checkbox>
+					</v-card-text>
 
-				<v-divider></v-divider>
+					<v-divider></v-divider>
 
-				<v-card-actions>
-					<v-btn color="secondary" text @click="close"> Close </v-btn>
-					<v-spacer></v-spacer>
-					<v-btn color="primary" text @click="confirm"> I accept </v-btn>
-				</v-card-actions>
+					<v-card-actions>
+						<v-btn color="secondary" text @click="close"> Close </v-btn>
+						<v-spacer></v-spacer>
+						<v-btn color="primary" type="submit" text> I accept </v-btn>
+					</v-card-actions>
+				</v-form>
 			</v-card>
 		</v-dialog>
 	</div>
