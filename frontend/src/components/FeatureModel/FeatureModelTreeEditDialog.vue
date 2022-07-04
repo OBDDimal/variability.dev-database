a3035a33a81c8ad7c6247035d6b82cc2b0effa90
Simon Karrer <simonkrrr3@googlemail.com>
2022-07-04 14:40:03 +0200

    Fixed discarding

--- frontend/src/components/FeatureModel/FeatureModelTreeEditDialog.vue
+++ frontend/src/components/FeatureModel/FeatureModelTreeEditDialog.vue
@@ -2,21 +2,26 @@
 	<div class="text-center">
 		<v-dialog width="500" v-model="show" persistent>
 			<v-card>
-				<v-card-title class="text-h5 grey lighten-2"> Edit Node </v-card-title>
+				<v-card-title class="text-h5 grey lighten-2"> Edit Feature </v-card-title>
 
-				<v-form @submit.prevent="confirm">
+				<v-form @submit.prevent="save">
 					<v-card-text>
 						<v-row class="my-2">
-							<v-col cols="12">
+							<v-col cols="12" class="pt-0">
 								<template>
-									<v-text-field v-model="name" :rules="[(value) => !!value || 'Required.']"></v-text-field>
+									<v-text-field
+										v-model="name"
+										hide-details
+										label="Name"
+										:rules="[(value) => !!value || 'Required.']"
+									></v-text-field>
 								</template>
 							</v-col>
 						</v-row>
 
-						<v-row class="my-2">
+						<v-row class="my-2" v-if="showGroupTypeSelection">
 							<v-col cols="12">
-								<v-btn-toggle v-if="showGroupTypeSelection" v-model="groupType" mandatory>
+								<v-btn-toggle dense v-model="convertGroupType" mandatory>
 									<v-btn>⊻ alt </v-btn>
 									<v-btn>∨ or </v-btn>
 									<v-btn>∧ and </v-btn>
@@ -24,9 +29,9 @@
 							</v-col>
 						</v-row>
 
-						<v-row class="my-2">
+						<v-row class="my-2" v-if="showMandatorySelection">
 							<v-col cols="12">
-								<v-btn-toggle v-if="showMandatorySelection" v-model="mandatory" mandatory>
+								<v-btn-toggle dense v-model="mandatory" mandatory>
 									<v-btn> mandatory </v-btn>
 									<v-btn> optional </v-btn>
 								</v-btn-toggle>
@@ -39,9 +44,9 @@
 					<v-divider></v-divider>
 
 					<v-card-actions>
-						<v-btn color="secondary" text @click="close"> Close </v-btn>
+						<v-btn color="secondary" text @click="discard"> Discard </v-btn>
 						<v-spacer></v-spacer>
-						<v-btn color="primary" type="submit" text> I accept </v-btn>
+						<v-btn color="primary" type="submit" text> Save </v-btn>
 					</v-card-actions>
 				</v-form>
 			</v-card>
@@ -55,23 +60,29 @@ import Vue from 'vue';
 export default Vue.extend({
 	name: 'FeatureModelTreeEditDialog',
 
+	data: () => ({
+		name: undefined,
+		groupType: undefined,
+		mandatory: undefined,
+		abstract: undefined,
+	}),
+
 	props: {
 		d3Node: Object,
 	},
 
-	computed: {
-		name: {
-			get() {
-				if (this.d3Node) {
-					return this.d3Node.data.name;
-				}
-				return 0;
-			},
-			set(newValue) {
-				this.d3Node.data.name = newValue;
-			},
+	watch: {
+		d3Node() {
+			if (this.d3Node) {
+				this.name = this.d3Node.data.name;
+				this.groupType = this.d3Node.data.groupType;
+				this.mandatory = this.d3Node.data.isMandatory;
+				this.abstract = this.d3Node.data.isAbstract;
+			}
 		},
+	},
 
+	computed: {
 		show: {
 			get() {
 				return !!this.d3Node;
@@ -79,10 +90,10 @@ export default Vue.extend({
 			set() {},
 		},
 
-		groupType: {
+		convertGroupType: {
 			get() {
 				if (this.d3Node) {
-					switch (this.d3Node.data.groupType) {
+					switch (this.groupType) {
 						case 'alt':
 							return 0;
 						case 'or':
@@ -98,30 +109,18 @@ export default Vue.extend({
 			set(newValue) {
 				switch (newValue) {
 					case 0:
-						this.d3Node.data.groupType = 'alt';
+						this.groupType = 'alt';
 						break;
 					case 1:
-						this.d3Node.data.groupType = 'or';
+						this.groupType = 'or';
 						break;
 					case 2:
-						this.d3Node.data.groupType = 'and';
+						this.groupType = 'and';
 						break;
 					default:
-						this.d3Node.data.groupType = 'alt';
+						this.groupType = 'alt';
 						break;
 				}
-			},
-		},
-
-		mandatory: {
-			get() {
-				if (this.d3Node) {
-					return this.d3Node.data.isMandatory ? 0 : 1;
-				}
-				return 0;
-			},
-			set(newValue) {
-				this.d3Node.data.isMandatory = newValue == 0;
 			},
 		},
 
@@ -138,27 +137,23 @@ export default Vue.extend({
 			}
 			return false;
 		},
-
-		abstract: {
-			get() {
-				if (this.d3Node) {
-					return this.d3Node.data.isAbstract;
-				}
-				return false;
-			},
-			set(newValue) {
-				this.d3Node.data.isAbstract = newValue;
-			},
-		},
 	},
 
 	methods: {
+		discard() {
+			this.close();
+		},
+
 		close() {
 			this.show = false;
 			this.$emit('close');
 		},
 
-		confirm() {
+		save() {
+			this.d3Node.data.name = this.name;
+			this.d3Node.data.groupType = this.groupType;
+			this.d3Node.data.isMandatory = this.mandatory;
+			this.d3Node.data.isAbstract = this.abstract;
 			this.close();
 			this.$emit('update');
 		},
