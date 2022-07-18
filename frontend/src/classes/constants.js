8111ad2af8ce788abd8fc7f0c934ca1e42c46389
simonkrrr3 <simonkrrr3@googlemail.com>
2022-07-18 10:27:08 +0200

    Removed two unused variables from constants

--- frontend/src/classes/constants.js
+++ frontend/src/classes/constants.js
@@ -10,14 +10,10 @@ export const RECT_HEIGHT = 35;
 export const MONOSPACE_HEIGHT_WIDTH_FACTOR = 0.6;
 export const MANDATORY_CIRCLE_RADIUS = 6;
 export const GROUP_SEGMENT_RADIUS = 25; // Radius of the segment that represents the 'alt' and 'and' groups.
-export const CHILDREN_COUNT_LETTERS_TO_RADIUS = 11.5 * MONOSPACE_HEIGHT_WIDTH_FACTOR;
-
 export const FEATURE_FONT_SIZE = 16;
 export const CHILDREN_COUNT_FONT_SIZE = 7;
 
 export const SPACE_BETWEEN_NODES_HORIZONTALLY = 20;
-
-export let nodeIdCounter = 0;
 
 export const PSEUDO_NODE_SIZE = 20;
 
