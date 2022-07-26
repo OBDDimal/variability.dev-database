1d80b99ff4f8e5dabd2b40335c5f52297899add7
simonkrrr3 <simonkrrr3@googlemail.com>
2022-07-26 15:47:37 +0200

    Read initial tags of xml file: featureOrder, comments, calculations and properties

--- frontend/src/views/FeatureModel.vue
+++ frontend/src/views/FeatureModel.vue
@@ -1,206 +1,237 @@
 <template>
-	<div>
-		<feature-model-tree :rootNode="rootNode" @exportToXML="exportToXML" ref="featureModelTree"></feature-model-tree>
-		<v-btn
-			icon
-			x-large
-			elevation="2"
-			style="background-color: var(--v-primary-base)"
-			dark
-			absolute
-			bottom
-			right
-			@click="$store.commit('openConstraints', true)"
-			><v-icon>mdi-format-list-checks</v-icon></v-btn
-		>
-		<constraints :constraints="constraints" @update-feature-model="updateFeatureModel"></constraints>
-	</div>
+    <div>
+        <feature-model-tree ref="featureModelTree" :rootNode="rootNode" @exportToXML="exportToXML"></feature-model-tree>
+        <v-btn
+            absolute
+            bottom
+            dark
+            elevation="2"
+            icon
+            right
+            style="background-color: var(--v-primary-base)"
+            x-large
+            @click="$store.commit('openConstraints', true)"
+        >
+            <v-icon>mdi-format-list-checks</v-icon>
+        </v-btn
+        >
+        <constraints :constraints="constraints" @update-feature-model="updateFeatureModel"></constraints>
+    </div>
 </template>
 
 <script>
 import Vue from 'vue';
 import FeatureModelTree from '../components/FeatureModel/FeatureModelTree.vue';
 import Constraints from '../components/Constraints.vue';
-import { Constraint, VarConstraint } from '@/classes/constraint';
-import {littleModel} from '@/classes/featureModelData';
-import { FeatureNode } from '@/classes/featureNode';
+import {Constraint, VarConstraint} from '@/classes/constraint';
+import {berkeley} from '@/classes/featureModelData';
+import {FeatureNode} from '@/classes/featureNode';
 import * as update from "@/services/FeatureModel/update.service";
 import * as collapse from "@/services/FeatureModel/collapse.service";
 
 export default Vue.extend({
-	name: 'FeatureModel',
+    name: 'FeatureModel',
 
-	components: {
-		FeatureModelTree,
-		Constraints,
-	},
+    components: {
+        FeatureModelTree,
+        Constraints,
+    },
 
-	props: {},
+    props: {},
 
-	data: () => ({
-		featureMap: [],
-		constraints: [],
+    data: () => ({
+        featureMap: [],
+        constraints: [],
         properties: [],
-		rootNode: undefined,
-	}),
+        rootNode: undefined,
+    }),
 
-	created() {
-		// TODO: Axios request for xml
+    created() {
+        // TODO: Axios request for xml
 
-		const json = this.xmlToJson(littleModel);
-		this.rootNode = json.rootNode;
-		this.constraints = json.constraints;
+        const json = this.xmlToJson(berkeley);
+        this.rootNode = json.rootNode;
+        this.constraints = json.constraints;
         this.properties = json.properties;
-	},
+    },
 
-	computed: {},
+    computed: {},
 
-	methods: {
-		updateFeatureModel() {
-			collapse.update(this.$refs.featureModelTree.d3Data);
-			update.updateSvg(this.$refs.featureModelTree.d3Data);
-		},
+    methods: {
+        updateFeatureModel() {
+            collapse.update(this.$refs.featureModelTree.d3Data);
+            update.updateSvg(this.$refs.featureModelTree.d3Data);
+        },
 
-		xmlToJson(currentModel) {
-			const start = performance.now();
+        xmlToJson(currentModel) {
+            const start = performance.now();
 
-			// To remove the <?xml...?> line
-			let m = currentModel.split('\n').splice(1).join('\n');
+            // To remove the <?xml...?> line
+            let m = currentModel.split('\n').splice(1).join('\n');
 
-			const parser = new DOMParser();
-			const xmlDocument = parser.parseFromString(m, 'text/xml');
+            const parser = new DOMParser();
+            const xmlDocument = parser.parseFromString(m, 'text/xml');
 
-			const struct = xmlDocument.querySelector('struct');
-			const constraints = xmlDocument.querySelector('constraints');
-            const properties = xmlDocument.querySelector('properties');
+            const struct = xmlDocument.querySelector('struct');
+            const constraintsContainer = xmlDocument.querySelector('constraints');
+            const propertiesSection = xmlDocument.querySelector('properties');
+            const calculationsSection = xmlDocument.querySelector('calculations');
+            const commentsSection = xmlDocument.querySelector('comments');
+            const featureOrderSection = xmlDocument.querySelector('featureOrder');
 
-			const toReturn =  {
+            const toReturn = {
                 rootNode: this.getChildrenOfFeature(struct, null)[0],
-                constraints: this.getConstraints(constraints),
-                properties: this.getProperties(properties)
+                constraints: this.getConstraints(constraintsContainer),
+                properties: this.getProperties(propertiesSection),
+                calculations: this.getCalculations(calculationsSection),
+                comments: this.getComments(commentsSection),
+                featureOrder: this.getFeatureOrder(featureOrderSection),
             }
+            console.log(toReturn);
             console.log('Parsertime', performance.now() - start);
-            return toReturn;
-		},
-
-		getChildrenOfFeature(struct, parent) {
-			let toReturn = [];
-
-			for (const child of struct.childNodes) {
-				// To remove #text nodes, as they don't have a tagName
-				if (child.tagName) {
-					let toAppend = new FeatureNode(
-						parent,
-						child.getAttribute('name'),
-						child.tagName,
-						child.getAttribute('mandatory') === 'true',
-						child.getAttribute('abstract') === 'true'
-					);
-					toAppend.children = this.getChildrenOfFeature(child, toAppend);
-
-					this.featureMap[toAppend.name] = toAppend;
-					toReturn.push(toAppend);
-				}
-			}
-
-			return toReturn;
-		},
-
-		getConstraints(constraints) {
-			let toReturn = [];
-
-			for (const rule of constraints.childNodes) {
-				// To remove #text nodes, as they don't have a tagName
-				if (rule.tagName) {
-					const constraint = new Constraint([...rule.childNodes].filter((e) => e.tagName)[0], this.featureMap);
-					toReturn.push(constraint);
-				}
-			}
-			return toReturn;
-		},
-
-        getProperties(properties) {
-            let toReturn = [];
-
-            for (const prop of properties.childNodes) {
-                // To remove #text nodes, as they don't have a tagName
-                if (prop.tagName) {
-                    const property = {
-                        tag: prop.tagName,
-                        key: prop.getAttribute('key'),
-                        value: prop.getAttribute('value')
-                    };
-                    toReturn.push(property);
-                }
-            }
             return toReturn;
         },
 
-		exportToXML() {
-			let root = {};
+        getChildrenOfFeature(struct, parent) {
+            let toReturn = [];
 
-			Object.entries(this.featureMap).forEach(([, node]) => {
-				if (node.isRoot) {
-					root = node;
-				}
-			});
+            for (const child of struct.childNodes) {
+                // To remove #text nodes, as they don't have a tagName
+                if (child.tagName) {
+                    let toAppend = new FeatureNode(
+                        parent,
+                        child.getAttribute('name'),
+                        child.tagName,
+                        child.getAttribute('mandatory') === 'true',
+                        child.getAttribute('abstract') === 'true'
+                    );
+                    toAppend.children = this.getChildrenOfFeature(child, toAppend);
 
-			let xml = `<?xml version="1.0" encoding="UTF-8" standalone="no"?><featureModel>`;
+                    this.featureMap[toAppend.name] = toAppend;
+                    toReturn.push(toAppend);
+                }
+            }
+
+            return toReturn;
+        },
+
+        getConstraints(constraints) {
+            if (!constraints) return null;
+
+            return [...constraints.childNodes]
+                .filter((element) => element.tagName)
+                .map((element) => new Constraint([...element.childNodes].filter((e) => e.tagName)[0], this.featureMap));
+        },
+
+        getProperties(properties) {
+            if (!properties) return null;
+
+            return [...properties.childNodes]
+                .filter((element) => element.tagName)
+                .map((element) => ({
+                    tag: element.tagName,
+                    key: element.getAttribute('key'),
+                    value: element.getAttribute('value')
+                }));
+        },
+
+        getCalculations(calculationsSection) {
+            if (!calculationsSection) return null;
+
+            return {
+                Auto: calculationsSection.getAttribute('Auto'),
+                Constraints: calculationsSection.getAttribute('Constraints'),
+                Features: calculationsSection.getAttribute('Features'),
+                Redundant: calculationsSection.getAttribute('Redundant'),
+                Tautology: calculationsSection.getAttribute('Tautology'),
+            };
+        },
+
+        getComments(commentsSection) {
+            if (!commentsSection) return null;
+
+            return [...commentsSection.childNodes]
+                .filter((element) => element.tagName)
+                .map((element) => element.innerHTML);
+        },
+
+        getFeatureOrder(featureOrder) {
+            if (!featureOrder) return null;
+
+            return {
+                userDefined: featureOrder.getAttribute('userDefined'),
+            };
+        },
+
+        exportToXML() {
+            let root = {};
+
+            Object.entries(this.featureMap).forEach(([, node]) => {
+                if (node.isRoot) {
+                    root = node;
+                }
+            });
+
+            let xml = `<?xml version="1.0" encoding="UTF-8" standalone="no"?><featureModel>`;
+
             xml += `<properties>${this.properties.reduce(
                 (prev, prop) => prev + `<${prop.tag} key="${prop.key}" value="${prop.value}"/>`,
                 ''
             )}</properties>`;
-			xml += `<struct>${this.nodeToXML(root)}</struct>`;
-			xml += `<constraints>${this.constraints.reduce(
-				(prev, constraint) => prev + '<rule>' + this.constraintToXML(constraint) + '</rule>',
-				''
-			)}</constraints>`;
-			xml += `</featureModel>`;
 
-			const filename = 'featureModel.xml';
-			const pom = document.createElement('a');
-			const bb = new Blob([xml], { type: 'application/xml' });
+            xml += `<struct>${this.nodeToXML(root)}</struct>`;
 
-			pom.setAttribute('href', window.URL.createObjectURL(bb));
-			pom.setAttribute('download', filename);
+            xml += `<constraints>${this.constraints.reduce(
+                (prev, constraint) => prev + '<rule>' + this.constraintToXML(constraint) + '</rule>',
+                ''
+            )}</constraints>`;
 
-			pom.dataset.downloadurl = ['application/xml', pom.download, pom.href].join(':');
+            xml += `</featureModel>`;
 
-			pom.click();
-		},
+            const filename = 'featureModel.xml';
+            const pom = document.createElement('a');
+            const bb = new Blob([xml], {type: 'application/xml'});
 
-		nodeToXML(node) {
-			if (node.isLeaf()) {
-				return `<feature ${node.isAbstract ? 'abstract="true" ' : ''}${node.isMandatory ? 'mandatory="true" ' : ''}name="${
-					node.name
-				}"/>`;
-			} else {
-				let toReturn = `<${node.groupType} ${node.isAbstract ? 'abstract="true" ' : ''}${
-					node.isMandatory ? 'mandatory="true" ' : ''
-				}name="${node.name}">`;
+            pom.setAttribute('href', window.URL.createObjectURL(bb));
+            pom.setAttribute('download', filename);
 
-				node.children.forEach((childNode) => {
-					toReturn += this.nodeToXML(childNode);
-				});
+            pom.dataset.downloadurl = ['application/xml', pom.download, pom.href].join(':');
 
-				toReturn += `</${node.groupType}>`;
-				return toReturn;
-			}
-		},
+            pom.click();
+        },
 
-		constraintToXML(constraint) {
-			if (constraint instanceof VarConstraint) {
-				return `<var>${constraint.featureNode.name}</var>`;
-			} else if (constraint instanceof Constraint) {
-				let toReturn = `<${constraint.xmlOperator}>`;
-				constraint.children.forEach((childConstraint) => {
-					toReturn += this.constraintToXML(childConstraint);
-				});
-				toReturn += `</${constraint.xmlOperator}>`;
-				return toReturn;
-			}
-		},
-	},
+        nodeToXML(node) {
+            if (node.isLeaf()) {
+                return `<feature ${node.isAbstract ? 'abstract="true" ' : ''}${node.isMandatory ? 'mandatory="true" ' : ''}name="${
+                    node.name
+                }"/>`;
+            } else {
+                let toReturn = `<${node.groupType} ${node.isAbstract ? 'abstract="true" ' : ''}${
+                    node.isMandatory ? 'mandatory="true" ' : ''
+                }name="${node.name}">`;
+
+                node.children.forEach((childNode) => {
+                    toReturn += this.nodeToXML(childNode);
+                });
+
+                toReturn += `</${node.groupType}>`;
+                return toReturn;
+            }
+        },
+
+        constraintToXML(constraint) {
+            if (constraint instanceof VarConstraint) {
+                return `<var>${constraint.featureNode.name}</var>`;
+            } else if (constraint instanceof Constraint) {
+                let toReturn = `<${constraint.xmlOperator}>`;
+                constraint.children.forEach((childConstraint) => {
+                    toReturn += this.constraintToXML(childConstraint);
+                });
+                toReturn += `</${constraint.xmlOperator}>`;
+                return toReturn;
+            }
+        },
+    },
 });
 </script>
 
