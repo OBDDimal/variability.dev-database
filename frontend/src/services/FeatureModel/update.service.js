ac27ab87aca0bf81ccd7f3637ce11445252ea249
simonkrrr3 <simonkrrr3@googlemail.com>
2022-07-29 18:39:37 +0200

    Refactored update.service.js to fulfill sonarlint

--- frontend/src/services/FeatureModel/update.service.js
+++ frontend/src/services/FeatureModel/update.service.js
@@ -7,8 +7,8 @@ import * as count from "@/services/FeatureModel/count.service";
 
 function updateFeatureNodes(d3Data, visibleD3Nodes) {
     const featureNode = d3Data.container.featureNodesContainer.selectAll('g.node').data(
-        visibleD3Nodes.filter((d3Node) => d3Node.data instanceof FeatureNode),
-        (d3Node) => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter)
+        visibleD3Nodes.filter(d3Node => d3Node.data instanceof FeatureNode),
+        d3Node => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter),
     );
 
     // Enter new nodes
@@ -34,8 +34,8 @@ function updateFeatureNodes(d3Data, visibleD3Nodes) {
     featureNodeEnter.append('circle').classed('and-group-circle', true).attr('r', CONSTANTS.MANDATORY_CIRCLE_RADIUS);
 
     const pseudoNode = d3Data.container.featureNodesContainer.selectAll('g.pseudo-node').data(
-        visibleD3Nodes.filter((d3Node) => d3Node.data instanceof PseudoNode),
-        (d3Node) => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter)
+        visibleD3Nodes.filter(d3Node => d3Node.data instanceof PseudoNode),
+        d3Node => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter),
     );
     const pseudoNodeEnter = pseudoNode
         .enter()
@@ -50,33 +50,33 @@ function updateFeatureNodes(d3Data, visibleD3Nodes) {
 
     // Update nodes
     const featureNodeUpdate = featureNodeEnter.merge(featureNode);
-    featureNodeUpdate.attr('transform', (d3Node) => 'translate(' + d3Node.x + ', ' + d3Node.y + ')');
+    featureNodeUpdate.attr('transform', d3Node => `translate(${d3Node.x}, ${d3Node.y})`);
     featureNodeUpdate
         .select('.and-group-circle')
-        .classed('mandatory-and-group-circle', (d3Node) => d3Node.parent && d3Node.parent.data.isAnd() && d3Node.data.isMandatory)
-        .classed('optional-and-group-circle', (d3Node) => d3Node.parent && d3Node.parent.data.isAnd() && !d3Node.data.isMandatory);
+        .classed('mandatory-and-group-circle', d3Node => d3Node.parent && d3Node.parent.data.isAnd() && d3Node.data.isMandatory)
+        .classed('optional-and-group-circle', d3Node => d3Node.parent && d3Node.parent.data.isAnd() && !d3Node.data.isMandatory);
 
     const rectAndTextUpdate = featureNodeUpdate.select('.rect-and-text');
     rectAndTextUpdate
         .select('rect')
-        .classed('is-searched-feature', (d3Node) => d3Node.data.isSearched)
-        .attr('fill', (d3Node) => d3Node.data.color())
-        .attr('x', (d3Node) => -calcRectWidth(d3Data, d3Node) / 2)
-        .attr('width', (d3Node) => calcRectWidth(d3Data, d3Node));
+        .classed('is-searched-feature', d3Node => d3Node.data.isSearched)
+        .attr('fill', d3Node => d3Node.data.color())
+        .attr('x', d3Node => -calcRectWidth(d3Data, d3Node) / 2)
+        .attr('width', d3Node => calcRectWidth(d3Data, d3Node));
     rectAndTextUpdate
         .select('text')
-        .attr('font-style', (d3Node) => (d3Node.data.isAbstract ? 'italic' : 'normal'))
-        .classed('whiteText', (d3Node) => {
+        .attr('font-style', d3Node => d3Node.data.isAbstract ? 'italic' : 'normal')
+        .classed('whiteText', d3Node => {
             let color = d3Node.data.color();
             const rgb = color.replace(/[^\d,]/g, '').split(',');
             return rgb[0] * 0.299 + rgb[1] * 0.587 + rgb[2] * 0.114 <= 186;
         })
-        .text((d3Node) => (d3Data.isShortenedName ? d3Node.data.displayName : d3Node.data.name));
+        .text(d3Node => d3Data.isShortenedName ? d3Node.data.displayName : d3Node.data.name);
 
     // Enter triangle with number of direct and total children.
     const childrenCount = featureNodeUpdate.selectAll('g.children-count').data(
-        (d) => (d.data.isLeaf() ? [] : [d]),
-        (d) => d.id
+        d => d.data.isLeaf() ? [] : [d],
+        d => d.id,
     );
 
     const childrenCountEnter = childrenCount.enter().append('g').classed('children-count', true);
@@ -95,14 +95,14 @@ function updateFeatureNodes(d3Data, visibleD3Nodes) {
         .attr('font-size', CONSTANTS.CHILDREN_COUNT_FONT_SIZE);
 
     const childrenCountUpdate = childrenCountEnter.merge(childrenCount);
-    childrenCountUpdate.attr('transform', (d3Node) => 'translate(' + calcRectWidth(d3Data, d3Node) / 2 + ', ' + CONSTANTS.RECT_HEIGHT + ')');
-    childrenCountUpdate.selectAll('text.direct-children').text((d3Node) => d3Node.data.childrenCount());
-    childrenCountUpdate.selectAll('text.total-children').text((d3Node) => d3Node.data.totalSubnodesCount());
+    childrenCountUpdate.attr('transform', d3Node => `translate(${calcRectWidth(d3Data, d3Node) / 2}, ${CONSTANTS.RECT_HEIGHT})`);
+    childrenCountUpdate.selectAll('text.direct-children').text(d3Node => d3Node.data.childrenCount());
+    childrenCountUpdate.selectAll('text.total-children').text(d3Node => d3Node.data.totalSubnodesCount());
 
     childrenCount.exit().remove();
 
     const pseudoNodeUpdate = pseudoNodeEnter.merge(pseudoNode);
-    pseudoNodeUpdate.attr('transform', (d3Node) => 'translate(' + d3Node.x + ', ' + (d3Node.y + CONSTANTS.RECT_HEIGHT / 2) + ')');
+    pseudoNodeUpdate.attr('transform', d3Node => `translate(${d3Node.x}, ${d3Node.y + CONSTANTS.RECT_HEIGHT / 2})`);
 
     // Remove old/invisible nodes.
     featureNode.exit().remove();
@@ -111,8 +111,8 @@ function updateFeatureNodes(d3Data, visibleD3Nodes) {
 
 function updateHighlightedConstraints(d3Data, visibleD3Nodes) {
     const highlightedConstraintNodes = d3Data.container.highlightedConstraintsContainer.selectAll('g.highlighted-constraints').data(
-        visibleD3Nodes.filter((d3Node) => d3Node.data instanceof FeatureNode && d3Node.data.constraintsHighlighted.length),
-        (d3Node) => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter)
+        visibleD3Nodes.filter(d3Node => d3Node.data instanceof FeatureNode && d3Node.data.constraintsHighlighted.length),
+        d3Node => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter),
     );
 
     const highlightedConstraintNodesEnter = highlightedConstraintNodes.enter().append('g').classed('highlighted-constraints', true);
@@ -121,39 +121,38 @@ function updateHighlightedConstraints(d3Data, visibleD3Nodes) {
         .merge(highlightedConstraintNodes)
         .selectAll('rect')
         .data(
-            (d3Node) =>
-                d3Node.data.constraintsHighlighted.map((c) => ({
+            d3Node =>
+                d3Node.data.constraintsHighlighted.map(c => ({
                     constraint: c,
                     d3Node: d3Node,
                 })),
-            (json) => json.constraint.toString() + json.d3Node.id
+            json => json.constraint.toString() + json.d3Node.id,
         );
 
     // Enter highlighted constraint rects
     const highlightedConstraintNodeRectsEnter = highlightedConstraintNodeRects
         .enter()
         .append('rect')
-        .attr('stroke', (json) => json.constraint.color)
+        .attr('stroke', json => json.constraint.color)
         .attr('stroke-width', CONSTANTS.STROKE_WIDTH_CONSTANT)
         .attr('fill', 'transparent');
 
     // Update highlighted constraint rects
     highlightedConstraintNodeRectsEnter
         .merge(highlightedConstraintNodeRects)
-        .attr('x', (constraint) => -calcRectWidth(d3Data, constraint.d3Node) / 2)
-        .attr('height', (_, i) => CONSTANTS.RECT_HEIGHT + i * 2 * CONSTANTS.STROKE_WIDTH_CONSTANT + CONSTANTS.STROKE_WIDTH_CONSTANT)
+        .attr('x', constraint => -calcRectWidth(d3Data, constraint.d3Node) / 2)
+        .attr('height', (_, i) => CONSTANTS.RECT_HEIGHT
+            + i * 2 * CONSTANTS.STROKE_WIDTH_CONSTANT + CONSTANTS.STROKE_WIDTH_CONSTANT)
         .attr(
             'width',
-            (constraint, i) => calcRectWidth(d3Data, constraint.d3Node) + i * 2 * CONSTANTS.STROKE_WIDTH_CONSTANT + CONSTANTS.STROKE_WIDTH_CONSTANT
+            (constraint, i) => calcRectWidth(d3Data, constraint.d3Node)
+                + i * 2 * CONSTANTS.STROKE_WIDTH_CONSTANT + CONSTANTS.STROKE_WIDTH_CONSTANT,
         )
         .attr(
             'transform',
             (json, i) =>
-                'translate(' +
-                (json.d3Node.x - i * CONSTANTS.STROKE_WIDTH_CONSTANT - CONSTANTS.STROKE_WIDTH_CONSTANT / 2) +
-                ', ' +
-                (json.d3Node.y - i * CONSTANTS.STROKE_WIDTH_CONSTANT - CONSTANTS.STROKE_WIDTH_CONSTANT / 2) +
-                ')'
+                `translate(${json.d3Node.x - i * CONSTANTS.STROKE_WIDTH_CONSTANT - CONSTANTS.STROKE_WIDTH_CONSTANT / 2}, 
+                ${json.d3Node.y - i * CONSTANTS.STROKE_WIDTH_CONSTANT - CONSTANTS.STROKE_WIDTH_CONSTANT / 2})`,
         );
 
     // Remove constraints highlighted nodes
@@ -162,21 +161,23 @@ function updateHighlightedConstraints(d3Data, visibleD3Nodes) {
 }
 
 function updateLinks(d3Data, visibleD3Nodes) {
-    const links = visibleD3Nodes.slice(1).filter((d3Node) => d3Node.data instanceof FeatureNode);
-    const link = d3Data.container.linksContainer.selectAll('path.link').data(links, (d3Node) => d3Node.id);
+    const links = visibleD3Nodes.slice(1).filter(d3Node => d3Node.data instanceof FeatureNode);
+    const link = d3Data.container.linksContainer.selectAll('path.link').data(links, d3Node => d3Node.id);
 
     const linkEnter = link.enter().insert('path', 'g').classed('link', true);
 
     const linkUpdate = linkEnter.merge(link);
     linkUpdate
-        .classed('is-searched-link', (d3Node) => d3Node.data.isSearched)
-        .attr('d', (d3Node) => createPaths.createLink(d3Node.parent, d3Node));
+        .classed('is-searched-link', d3Node => d3Node.data.isSearched)
+        .attr('d', d3Node => createPaths.createLink(d3Node.parent, d3Node));
 
     link.exit().remove();
 }
 
 function updateColoring(d3Data) {
-    if (!d3Data.updateTrigger.coloring) return;
+    if (!d3Data.updateTrigger.coloring) {
+        return;
+    }
 
     const allNodes = d3Data.root.data.descendants();
     count.colorNodes(allNodes, d3Data.coloringIndex);
@@ -185,8 +186,8 @@ function updateColoring(d3Data) {
 
 function updateSegments(d3Data, visibleD3Nodes) {
     const segment = d3Data.container.segmentsContainer.selectAll('path.segment').data(
-        visibleD3Nodes.filter((d3Node) => d3Node.data instanceof FeatureNode && (d3Node.data.isAlt() || d3Node.data.isOr())),
-        (d3Node) => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter)
+        visibleD3Nodes.filter(d3Node => d3Node.data instanceof FeatureNode && (d3Node.data.isAlt() || d3Node.data.isOr())),
+        d3Node => d3Node.id || (d3Node.id = ++d3Data.nodeIdCounter),
     );
 
     const segmentEnter = segment.enter().append('path').classed('segment', true);
@@ -194,10 +195,10 @@ function updateSegments(d3Data, visibleD3Nodes) {
     // Segment update.service.js
     segmentEnter
         .merge(segment)
-        .classed('alt-group', (d3Node) => d3Node.data.isAlt())
-        .classed('or-group', (d3Node) => d3Node.data.isOr())
-        .attr('d', (d3Node) => createPaths.createGroupSegment(d3Node, CONSTANTS.GROUP_SEGMENT_RADIUS))
-        .attr('transform', (d3Node) => 'translate(' + d3Node.x + ', ' + d3Node.y + ')');
+        .classed('alt-group', d3Node => d3Node.data.isAlt())
+        .classed('or-group', d3Node => d3Node.data.isOr())
+        .attr('d', d3Node => createPaths.createGroupSegment(d3Node, CONSTANTS.GROUP_SEGMENT_RADIUS))
+        .attr('transform', d3Node => `translate(${d3Node.x}, ${d3Node.y})`);
 
     segment.exit().remove();
 }
@@ -219,16 +220,17 @@ export function updateSvg(d3Data) {
 
 // Calculates rect-width dependent on font-size dynamically.
 export function calcRectWidth(d3Data, d3Node) {
+    let width;
     if (d3Node.data instanceof FeatureNode) {
-        return (
-            (d3Data.isShortenedName
-                ? d3Node.data.displayName.length
-                : d3Node.data.name.length) *
-            (CONSTANTS.FEATURE_FONT_SIZE * CONSTANTS.MONOSPACE_HEIGHT_WIDTH_FACTOR) +
-            CONSTANTS.RECT_MARGIN.left +
-            CONSTANTS.RECT_MARGIN.right
-        );
+        width = (d3Data.isShortenedName
+            ? d3Node.data.displayName.length
+            : d3Node.data.name.length) *
+        (CONSTANTS.FEATURE_FONT_SIZE * CONSTANTS.MONOSPACE_HEIGHT_WIDTH_FACTOR) +
+        CONSTANTS.RECT_MARGIN.left +
+        CONSTANTS.RECT_MARGIN.right;
     } else if (d3Node.data instanceof PseudoNode) {
-        return CONSTANTS.PSEUDO_NODE_SIZE * 2;
+        width = CONSTANTS.PSEUDO_NODE_SIZE * 2;
     }
+
+    return width;
 }
