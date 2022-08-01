c8845ffa931c421f68fc7fcc466f86fb8c32598a
Eixix <tobiasbetz@hotmail.de>
2022-08-01 14:19:03 +0200

    Gunicorn instead of uwsgi

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-while !</dev/tcp/db/5432; do echo "Connecting" && sleep 1; done;
+while ! nc -zv db 5432; do echo "Connecting" && sleep 1; done;
+
 python manage.py makemigrations
 python manage.py migrate --run-syncdb
-gunicorn --bind ":8000" --workers 3 ddueruem.wsgi:application
+gunicorn --bind ":8000" --workers 3 ddueruemweb.wsgi:application
+
