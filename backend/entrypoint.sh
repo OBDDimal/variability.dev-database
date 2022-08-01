3bc3d5e8db68184aac48d9949694ab852daf58ae
Eixix <tobiasbetz@hotmail.de>
2022-08-01 13:16:29 +0200

    Gunicorn try

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,4 @@
 while !</dev/tcp/db/5432; do echo "Connecting" && sleep 1; done;
 python manage.py makemigrations
 python manage.py migrate --run-syncdb
-uwsgi --module ddueruemweb.wsgi --master --processes 1 --threads 1 --socket /tmp/uwsgi.sock --chmod-socket=666 &
-nginx -g "daemon off;"
+gunicorn --bind ":8000" --workers 3 ddueruem.wsgi:application
