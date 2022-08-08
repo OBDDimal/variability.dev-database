faea7f664db55145dabe8ed5e11159c36e9ef29d
Eixix <tobiasbetz@hotmail.de>
2022-08-08 10:38:04 +0200

    PWA capabilities

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import VueFullscreen from "vue-fullscreen";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    themeColor: '#1976d2',
+    name: 'DDueruem Web'
+  }
 }
