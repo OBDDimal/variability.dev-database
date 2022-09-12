99ada0248be19bdcfd0a296da680fe3a53741f2d
Eixix <tobiasbetz@hotmail.de>
2022-09-12 11:46:01 +0200

    Revert "Fixed route switching with pwa"
    This reverts commit fb28fd6f938e8fa23c723ac6cfe8d0cf43ad822e.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-	transpileDependencies: ['vuetify'],
-	pwa: {
-		themeColor: '#1976d2',
-		name: 'DDueruem Web',
-		display: 'fullscreen',
-		workboxOptions: {
-			navigateFallback: 'index.html',
-		},
-	},
+  transpileDependencies: [
+    'vuetify'
+  ],
+  pwa: {
+    themeColor: '#1976d2',
+    name: 'DDueruem Web',
+    display: 'fullscreen'
+  }
 }
