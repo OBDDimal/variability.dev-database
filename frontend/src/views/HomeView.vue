84edf07e6ecdb6a886d28c60b47cbf3185d9b6c3
Lukas Harsch <lukas.harsch@uni-ulm.de>
2022-10-10 11:25:35 +0200

    Fixed bug after logging out

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,123 +1,121 @@
 <template>
-	<div class="mainView">
-		<h3 class="text-h3 mb-2 mt-8">Welcome to ddueruem</h3>
-		<h5 class="text-h5 mb-4">
-			A web service for sharing feature model instances and collaborative
-			benchmarking
-		</h5>
-		<feature-model-table
-			:items="$store.state.files"
-			:loading="loading"
-			:addable="true"
-		/>
-	</div>
+    <div class="mainView">
+        <h3 class="text-h3 mb-2 mt-8">Welcome to ddueruem</h3>
+        <h5 class="text-h5 mb-4">
+            A web service for sharing feature model instances and collaborative
+            benchmarking
+        </h5>
+        <feature-model-table
+            :items="$store.state.files"
+            :loading="loading"
+            :addable="true"
+        />
+    </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import FeatureModelTable from '@/components/FeatureModelTable'
+import Vue from 'vue';
+import FeatureModelTable from '@/components/FeatureModelTable';
 
 export default Vue.extend({
-	name: 'HomeView',
+    name: 'HomeView',
 
-	components: {
-		FeatureModelTable,
-	},
+    components: {
+        FeatureModelTable,
+    },
 
-	props: {},
+    props: {},
 
-	data: () => ({
-		search: '',
-		dialog: false,
-		createDialog: false,
-		editedIndex: -1,
-		headers: [
-			{
-				text: 'ID',
-				align: 'start',
-				sortable: false,
-				value: 'id',
-			},
-			{ text: 'Label', value: 'label' },
-			{ text: 'Description', value: 'description' },
-			{ text: 'License', value: 'license' },
-			{ text: 'Tags', value: 'tags' },
-			{ text: 'Uploaded on', value: 'uploaded' },
-			{
-				text: 'Actions',
-				align: 'center',
-				value: 'actions',
-				sortable: false,
-			},
-		],
-		editedItem: {
-			label: '',
-			description: '',
-			license: 'CC-BY Mention',
-			tags: null,
-			uploaded: 'Today',
-		},
-		defaultItem: {
-			label: '',
-			description: '',
-			license: 'CC-BY Mention',
-			tags: null,
-			uploaded: 'Today',
-		},
-		files: [],
-		licenses: [],
-		families: [],
-		tags: [],
-		check1: false,
-		check2: false,
-		check3: false,
-		loading: false,
-		info: '',
-	}),
+    data: () => ({
+        search: '',
+        dialog: false,
+        createDialog: false,
+        editedIndex: -1,
+        headers: [
+            {
+                text: 'ID',
+                align: 'start',
+                sortable: false,
+                value: 'id',
+            },
+            { text: 'Label', value: 'label' },
+            { text: 'Description', value: 'description' },
+            { text: 'License', value: 'license' },
+            { text: 'Tags', value: 'tags' },
+            { text: 'Uploaded on', value: 'uploaded' },
+            {
+                text: 'Actions',
+                align: 'center',
+                value: 'actions',
+                sortable: false,
+            },
+        ],
+        editedItem: {
+            label: '',
+            description: '',
+            license: 'CC-BY Mention',
+            tags: null,
+            uploaded: 'Today',
+        },
+        defaultItem: {
+            label: '',
+            description: '',
+            license: 'CC-BY Mention',
+            tags: null,
+            uploaded: 'Today',
+        },
+        licenses: [],
+        families: [],
+        tags: [],
+        check1: false,
+        check2: false,
+        check3: false,
+        loading: false,
+        info: '',
+    }),
 
-	computed: {
-		formTitle() {
-			return this.editedIndex === -1 ? 'Upload New Model' : 'Edit Model'
-		} /*
+    computed: {
+        formTitle() {
+            return this.editedIndex === -1 ? 'Upload New Model' : 'Edit Model';
+        } /*
     existingFamilies() {
       return this.desserts.map((x) => x.label);
     }, */,
-	},
+    },
 
-	watch: {
-		dialog(val) {
-			val || this.close()
-		},
-	},
+    watch: {
+        dialog(val) {
+            val || this.close();
+        },
+    },
 
-	methods: {
-		close() {
-			this.dialog = false
-			this.$nextTick(() => {
-				this.editedItem = Object.assign({}, this.defaultItem)
-				this.editedIndex = -1
-			})
-		},
+    methods: {
+        close() {
+            this.dialog = false;
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            });
+        },
 
-		save() {
-			/* if (this.editedIndex > -1) {
+        save() {
+            /* if (this.editedIndex > -1) {
               Object.assign(this.desserts[this.editedIndex], this.editedItem);
             } else {
               this.desserts.push(this.editedItem);
             } */
-			this.close()
-		},
-	},
+            this.close();
+        },
+    },
 
-	mounted() {
-		this.loading = true
-		this.$store.dispatch('fetchFamilies')
-		this.families = this.$store.state.families
-		this.$store.dispatch('fetchTags')
-		this.tags = this.$store.state.tags
-		this.$store.dispatch('fetchFiles')
-		this.files = this.$store.state.files
-		this.loading = false
-	},
-})
+    mounted() {
+        this.loading = true;
+        this.$store.dispatch('fetchFamilies');
+        this.families = this.$store.state.families;
+        this.$store.dispatch('fetchTags');
+        this.tags = this.$store.state.tags;
+        this.$store.dispatch('fetchFiles');
+        this.loading = false;
+    },
+});
 </script>
