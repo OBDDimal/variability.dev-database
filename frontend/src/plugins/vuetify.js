3f6686d0568cf69496d4ff19e2b19c3191b36399
Lukas Harsch <lukas.harsch@uni-ulm.de>
2022-10-24 12:10:26 +0200

    Changed dark mode opt-in to opt-out

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,10 +1,11 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
     theme: {
-        options: {customProperties: true},
+        dark: true,
+        options: { customProperties: true },
     },
 });
