adeb38909988686ab660306acdadcaf6b2e15b79
Simon Karrer <simonkrrr3@googlemail.com>
2023-04-04 13:59:02 +0200

    Defined structure of configurator

--- frontend/src/views/FeatureModelConfiguration.vue
+++ frontend/src/views/FeatureModelConfiguration.vue
@@ -1,69 +1,90 @@
 <template>
   <div class="mainView">
-    <v-data-table
-        :headers="headersVersions"
-        :items="featureModel.versions"
-        item-key="version"
-        show-select
-        single-select
-        class="elevation-1"
-    >
-      <template v-slot:item.data-table-select="{ item }">
-        <v-row>
-          <v-col cols="12" sm="4">
-            <v-simple-checkbox
-                :value="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ExplicitlySelected"
-                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                @input="select(item, SelectionState.ExplicitlySelected)"
-                color="green"
-            ></v-simple-checkbox>
-          </v-col>
-          <v-col cols="12" sm="4">
-            <v-simple-checkbox
-                :value="item.selectionState === SelectionState.ImplicitlyDeselected || item.selectionState === SelectionState.ExplicitlyDeselected"
-                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                @input="select(item, SelectionState.ExplicitlyDeselected)"
-                color="red"
-            ></v-simple-checkbox>
-          </v-col>
-        </v-row>
-      </template>
-    </v-data-table>
+    <v-row>
+
+      <!-- Versions -->
+      <v-col cols="12" sm="4">
+        <v-data-table
+            :headers="headersVersions"
+            :items="featureModel.versions"
+            item-key="version"
+            show-select
+            single-select
+            class="elevation-1"
+        >
+          <template v-slot:item.data-table-select="{ item }">
+            <v-row>
+              <v-col cols="12" sm="4">
+                <v-simple-checkbox
+                    :value="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ExplicitlySelected"
+                    :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    @input="select(item, SelectionState.ExplicitlySelected)"
+                    color="green"
+                ></v-simple-checkbox>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-simple-checkbox
+                    on-icon="mdi-close-box"
+                    :value="item.selectionState === SelectionState.ImplicitlyDeselected || item.selectionState === SelectionState.ExplicitlyDeselected"
+                    :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    @input="select(item, SelectionState.ExplicitlyDeselected)"
+                    color="red"
+                ></v-simple-checkbox>
+              </v-col>
+            </v-row>
+          </template>
+        </v-data-table>
+      </v-col>
 
 
-    <v-data-table
-        :headers="headersFeatures"
-        :items="featureModel.features"
-        item-key="name"
-        show-select
-        single-select
-        class="elevation-1"
-    >
-      <template v-slot:item.data-table-select="{ item }">
-        <v-row>
-          <v-col cols="12" sm="4">
-            <v-simple-checkbox
-                :value="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ExplicitlySelected"
-                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                @input="select(item, SelectionState.ExplicitlySelected)"
-                color="green"
-            ></v-simple-checkbox>
-          </v-col>
-          <v-col cols="12" sm="4">
-            <v-simple-checkbox
-                :value="item.selectionState === SelectionState.ImplicitlyDeselected || item.selectionState === SelectionState.ExplicitlyDeselected"
-                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
-                @input="select(item, SelectionState.ExplicitlyDeselected)"
-                color="red"
-            ></v-simple-checkbox>
-          </v-col>
-        </v-row>
-      </template>
-    </v-data-table>
+      <!-- All features -->
+      <v-col cols="12" sm="4">
+        <v-data-table
+            :headers="headersFeatures"
+            :items="featureModel.features"
+            item-key="name"
+            show-select
+            single-select
+            class="elevation-1"
+        >
+          <template v-slot:item.data-table-select="{ item }">
+            <v-row>
+              <v-col cols="12" sm="4">
+                <v-simple-checkbox
+                    :value="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ExplicitlySelected"
+                    :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    @input="select(item, SelectionState.ExplicitlySelected)"
+                    color="green"
+                ></v-simple-checkbox>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-simple-checkbox
+                    on-icon="mdi-close-box"
+                    :value="item.selectionState === SelectionState.ImplicitlyDeselected || item.selectionState === SelectionState.ExplicitlyDeselected"
+                    :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                    @input="select(item, SelectionState.ExplicitlyDeselected)"
+                    color="red"
+                ></v-simple-checkbox>
+              </v-col>
+            </v-row>
+          </template>
+        </v-data-table>
+      </v-col>
+
+      <!-- Tree hierarchy table for one selected version -->
+      <v-col cols="12" sm="4">
+
+      </v-col>
+    </v-row>
+
+    <!-- feature model viewer -->
+    <v-row>
+
+    </v-row>
   </div>
 </template>
 
@@ -84,7 +105,7 @@ export default Vue.extend({
   data: () => ({
     featureModel: FeatureModel,
     headersVersions: [{text: 'Version', value: 'version'}],
-    headersFeatures: [{text: 'Feature', value: 'name'}],
+    headersFeatures: [{text: 'All features', value: 'name'}],
   }),
 
   created() {
