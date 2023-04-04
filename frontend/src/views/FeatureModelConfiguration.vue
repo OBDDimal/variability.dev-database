4bd0f21f92e9c39381e532ca94849e418b7d38ec
Simon Karrer <simonkrrr3@googlemail.com>
2023-04-04 13:07:51 +0200

    Added buttons for selection and deselection of versions and features

--- frontend/src/views/FeatureModelConfiguration.vue
+++ frontend/src/views/FeatureModelConfiguration.vue
@@ -1,6 +1,69 @@
 <template>
   <div class="mainView">
+    <v-data-table
+        :headers="headersVersions"
+        :items="featureModel.versions"
+        item-key="version"
+        show-select
+        single-select
+        class="elevation-1"
+    >
+      <template v-slot:item.data-table-select="{ item }">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-simple-checkbox
+                :value="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ExplicitlySelected"
+                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                @input="select(item, SelectionState.ExplicitlySelected)"
+                color="green"
+            ></v-simple-checkbox>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-simple-checkbox
+                :value="item.selectionState === SelectionState.ImplicitlyDeselected || item.selectionState === SelectionState.ExplicitlyDeselected"
+                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                @input="select(item, SelectionState.ExplicitlyDeselected)"
+                color="red"
+            ></v-simple-checkbox>
+          </v-col>
+        </v-row>
+      </template>
+    </v-data-table>
 
+
+    <v-data-table
+        :headers="headersFeatures"
+        :items="featureModel.features"
+        item-key="name"
+        show-select
+        single-select
+        class="elevation-1"
+    >
+      <template v-slot:item.data-table-select="{ item }">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-simple-checkbox
+                :value="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ExplicitlySelected"
+                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                @input="select(item, SelectionState.ExplicitlySelected)"
+                color="green"
+            ></v-simple-checkbox>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-simple-checkbox
+                :value="item.selectionState === SelectionState.ImplicitlyDeselected || item.selectionState === SelectionState.ExplicitlyDeselected"
+                :readonly="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                :disabled="item.selectionState === SelectionState.ImplicitlySelected || item.selectionState === SelectionState.ImplicitlyDeselected"
+                @input="select(item, SelectionState.ExplicitlyDeselected)"
+                color="red"
+            ></v-simple-checkbox>
+          </v-col>
+        </v-row>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
@@ -8,41 +71,37 @@
 import Vue from 'vue';
 import {FeatureModel} from "@/classes/Configuration/FeatureModel";
 import {xmlVersions} from "@/classes/Configuration/example";
+import {SelectionState} from "@/classes/Configuration/SelectionState";
 
 export default Vue.extend({
   name: 'FeatureModelConfiguration',
-
-  props: {
-    id: undefined,
+  computed: {
+    SelectionState() {
+      return SelectionState
+    }
   },
 
   data: () => ({
-    data: {
-      featureModel: FeatureModel,
-    },
+    featureModel: FeatureModel,
+    headersVersions: [{text: 'Version', value: 'version'}],
+    headersFeatures: [{text: 'Feature', value: 'name'}],
   }),
 
   created() {
     this.initData();
   },
 
-  watch: {
-    selected() {
-    },
-  },
-
   methods: {
     initData() {
-        this.featureModel = FeatureModel.create(xmlVersions);
+      this.featureModel = FeatureModel.create(xmlVersions);
     },
 
-    update(selected) {
-      selected.isExplicit = selected.isSelected;
-      this.data.rootNode.decisionPropagation();
-    },
-
-    onOpen(opened) {
-      console.log(opened.map(n => n.name));
+    select(item, selectionState) {
+      if (item.selectionState === selectionState) {
+        item.selectionState = SelectionState.Unselected;
+      } else {
+        item.selectionState = selectionState;
+      }
     }
   },
 
