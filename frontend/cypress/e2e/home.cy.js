a9b72f0a64ef397f9fde5f53f66247910f47ede5
sbu <sebastian.budsa@profect.de>
2023-04-28 13:16:32 +0200

    - added multiple upload test - added zip upload test

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -64,7 +64,7 @@ describe('Home page tests', () => {
             )
         })
 
-        it.only(`Uploading a single feature model file should send a mail`, () => {
+        it(`Uploading a single feature model file should send a mail`, () => {
             cy.wait(['@getFilesUploadedConfirmed', '@getFamilies', '@getTags']).then(
                 (interceptions) => {
                     cy.intercept('GET', '/Licenses/').as('getLicenses');
@@ -85,6 +85,79 @@ describe('Home page tests', () => {
                     cy.intercept('POST', '/bulk-upload/').as('postBulkUpload');
 
                     cy.get('[data-cy="file-create-upload-button"]').click();
+                    cy.wait(['@postFamilies', '@postBulkUpload']).then(
+                        (interceptions2) => {
+                            //check for mail popup
+                            cy.contains('Check your mails', { matchCase: false }).should('exist'); //click tutorial away
+                        }
+                    )
+                }
+            )
+            //delete created family
+            if(Cypress.platform === 'win32'){
+                cy.exec('venv\\Scripts\\python.exe ../backend/manage.py runscript delete_test_family');
+            } else {
+                cy.exec("./venv/bin/python ../backend/manage.py runscript delete_test_family");
+            }
+        })
+
+        it(`Uploading multiple feature model files should send a mail`, () => {
+            cy.wait(['@getFilesUploadedConfirmed', '@getFamilies', '@getTags']).then(
+                (interceptions) => {
+                    cy.intercept('GET', '/Licenses/').as('getLicenses');
+
+                    cy.get('[data-cy="feature-model-table-upload-button"]').click();
+                    cy.get('[data-cy="file-create-multiple-upload-tab"]').click();
+                    cy.get('[data-cy="file-create-multiple-file-input"]').selectFile(['cypress/e2e/cypresstestfile.xml', 'cypress/e2e/cypresstestfile2.xml'], {force: true});
+                    cy.contains('2 files (752 B in total)', { matchCase: false }).should('exist');
+                    cy.get('[data-cy="file-create-multiple-license-select"]').click().type('{downArrow}{enter}', {force: true});
+                    cy.get('[data-cy="file-create-multiple-family-label-textfield"]').type('cypressfamilylabel{enter}');
+                    cy.get('[data-cy="file-create-multiple-family-description-textfield"]').type('cypressfamilydescription{enter}');
+                    cy.get('[data-cy="file-create-multiple-legal-share-checkbox"]').click({force: true});
+                    cy.get('[data-cy="file-create-multiple-user-data-checkbox"]').click({force: true});
+                    cy.get('[data-cy="file-create-multiple-open-source-checkbox"]').click({force: true});
+
+                    cy.intercept('POST', '/families/').as('postFamilies');
+                    cy.intercept('POST', '/bulk-upload/').as('postBulkUpload');
+
+                    cy.get('[data-cy="file-create-multiple-upload-button"]').click();
+                    cy.wait(['@postFamilies', '@postBulkUpload']).then(
+                        (interceptions2) => {
+                            //check for mail popup
+                            cy.contains('Check your mails', { matchCase: false }).should('exist'); //click tutorial away
+                        }
+                    )
+                }
+            )
+            //delete created family
+            if(Cypress.platform === 'win32'){
+                cy.exec('venv\\Scripts\\python.exe ../backend/manage.py runscript delete_test_family');
+            } else {
+                cy.exec("./venv/bin/python ../backend/manage.py runscript delete_test_family");
+            }
+        })
+
+        it.only(`Uploading a zip file should send a mail`, () => {
+            cy.wait(['@getFilesUploadedConfirmed', '@getFamilies', '@getTags']).then(
+                (interceptions) => {
+                    cy.intercept('GET', '/Licenses/').as('getLicenses');
+
+                    cy.get('[data-cy="feature-model-table-upload-button"]').click();
+                    cy.get('[data-cy="file-create-zip-upload-tab"]').click();
+                    cy.get('[data-cy="file-create-zip-label-textfield"]').type('cypresstestfilelabel');
+                    cy.get('[data-cy="file-create-zip-description-textfield"]').type('cypresstestfiledescription');
+                    cy.get('[data-cy="file-create-zip-file-input"]').selectFile('cypress/e2e/cypresstestarchive.zip', {force: true});
+                    cy.get('[data-cy="file-create-zip-license-select"]').click().type('{downArrow}{enter}', {force: true});
+                    cy.get('[data-cy="file-create-zip-family-label-textfield"]').type('cypressfamilylabel{enter}');
+                    cy.get('[data-cy="file-create-zip-family-description-textfield"]').type('cypressfamilydescription{enter}');
+                    cy.get('[data-cy="file-create-zip-legal-share-checkbox"]').click({force: true});
+                    cy.get('[data-cy="file-create-zip-user-data-checkbox"]').click({force: true});
+                    cy.get('[data-cy="file-create-zip-open-source-checkbox"]').click({force: true});
+
+                    cy.intercept('POST', '/families/').as('postFamilies');
+                    cy.intercept('POST', '/bulk-upload/').as('postBulkUpload');
+
+                    cy.get('[data-cy="file-create-zip-upload-button"]').click();
                     cy.wait(['@postFamilies', '@postBulkUpload']).then(
                         (interceptions2) => {
                             //check for mail popup
