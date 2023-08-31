96eadbcc1ff4ee63df993a73ae40bdd469c11f88
Lukas Ostheimer <131896156+LukasOsti@users.noreply.github.com>
2023-08-31 13:50:38 +0200

    fix: fixed bug that the features would still be colored even if service is not reachable

--- frontendVue3/src/services/FeatureModel/colorsFromService.service.js
+++ frontendVue3/src/services/FeatureModel/colorsFromService.service.js
@@ -1,15 +1,21 @@
 import { jsonToXML } from '@/services/xmlTranspiler.service';
-import axios from 'axios';
+import axios, { CancelToken } from 'axios';
 import { NODE_CORE_COLOR, NODE_DEAD_COLOR, NODE_FALSEOP_COLOR } from '@/classes/constants';
 import { useAppStore } from '@/store/app';
 
 export async function getColorsFromService(featureModel, d3Data) {
     try {
+        const source = CancelToken.source();
+        const timeout = setTimeout(() => {
+          source.cancel();
+          // Timeout Logic
+        }, 450);
         const content = new TextEncoder().encode(jsonToXML(featureModel));
         let response = await axios.post(`${import.meta.env.VITE_APP_DOMAIN_FEATUREIDESERVICE}stats`, {
           name: featureModel.id + ".xml",
           content: Array.from(content)
-        });
+        }, {cancelToken: source.token});
+        clearTimeout(timeout);
         let deadFeatures = response.data.deadFeatures;
         let falseOptionalFeatures = response.data.falseOptionalFeatures;
         let coreFeatures = response.data.coreFeatures;
@@ -17,13 +23,13 @@ export async function getColorsFromService(featureModel, d3Data) {
         if (coreFeatures.length > 0) {
             d3Data.root.descendants().filter(node => coreFeatures.includes(node.data.name)).forEach(node => node.data.setColor(NODE_CORE_COLOR));
         }
-        if (deadFeatures.length > 0) {
-            d3Data.root.descendants().filter(node => deadFeatures.includes(node.data.name)).forEach(node => node.data.setColor(NODE_DEAD_COLOR));
-        }
         if (falseOptionalFeatures.length > 0) {
             d3Data.root.descendants().filter(node => falseOptionalFeatures.includes(node.data.name)).forEach(node => node.data.setColor(NODE_FALSEOP_COLOR));
         }
-
+        if (deadFeatures.length > 0) {
+            d3Data.root.descendants().filter(node => deadFeatures.includes(node.data.name)).forEach(node => node.data.setColor(NODE_DEAD_COLOR));
+        }
+        return true;
     } catch (e) {
         const appStore = useAppStore()
         appStore.updateSnackbar(
@@ -31,5 +37,6 @@ export async function getColorsFromService(featureModel, d3Data) {
           'error',
           3000,
           true)
+        return false;
     }
 }
